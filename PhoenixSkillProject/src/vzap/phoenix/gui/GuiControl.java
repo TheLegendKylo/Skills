e3c47476bd326b0536dfa8e3ba9de2d74c9923d1
A208563 <A208563@41360JNBITD857L.za.sbicdirectory.com>
2018-01-17 14:11:28 +0200

    Removed Resizable for Ryan

--- PhoenixSkillProject/src/vzap/phoenix/gui/GuiControl.java
+++ PhoenixSkillProject/src/vzap/phoenix/gui/GuiControl.java
@@ -17,7 +17,7 @@ public class GuiControl extends JFrame
 		this.setTitle("Skills Application");
 		this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		this.setSize(1200,970);
-		this.setResizable(false);
+		//this.setResizable(false);
 		this.addWindowListener(new java.awt.event.WindowAdapter() 
 		{
 		    @Override
