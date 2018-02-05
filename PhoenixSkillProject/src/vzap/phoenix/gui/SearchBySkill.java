0ccd9aaece583b87adfd5580d9e812d751f7fb87
a043410 <a043410@20488jnbitd065l.za.sbicdirectory.com>
2018-02-05 11:41:01 +0200

    commit changes to lookup of skillcombobox

--- PhoenixSkillProject/src/vzap/phoenix/gui/SearchBySkill.java
+++ PhoenixSkillProject/src/vzap/phoenix/gui/SearchBySkill.java
@@ -178,24 +178,34 @@ public class SearchBySkill extends JPanel implements ActionListener
 
 			}		
 		}
-		
-//		if(source == clearBut)
-//		{
-//			skillTable = new JTable();
-//			skillScrollPane.setViewportView(skillTable);
-//		}
-//
+
 
 		if(source == skillComboBox)
 		{
 			empSkillList = new ArrayList<>();
 		
 //          store the index and skillId of the cell the user has chosen from the combobox 			
-			int x = skillComboBox.getSelectedIndex();
-			int y = skillList.get(x).getSkillId();
+			int comboSkill_pointer = skillComboBox.getSelectedIndex();
+			int skillList_pointer = 0;
+			System.out.println("searchbyskill - get skilldesc " + skillComboBox.getSelectedItem());
+			
+			
+//			System.out.println("searchby skill - value x " + x + " y " + y);
+			
+			for(int i = 0 ; i < skillList.size() ; i++)
+			{
+				System.out.println("searchbyskill - in for - skillist " + skillList.get(i).getSkillDescription());
+				System.out.println("searchbyskill - in for - skillComboBox " +skillComboBox.getSelectedItem() );
+				if(skillList.get(i).getSkillDescription() == skillComboBox.getSelectedItem())
+				{
+					skillList_pointer = i;
+					System.out.println("searchbyskill - skill found - " + skillList_pointer);
+					break;
+				}
+			}
 
 //          now go and search the EmployeeSkill table for all employees that have that skill			
-			empSkillList = clientControl.searchEmployeeSkill(y);
+			empSkillList = clientControl.searchEmployeeSkill(skillList_pointer+1);
 
 //          check if there are any matches for users search criteria 
 			if(empSkillList.size() == 0)
